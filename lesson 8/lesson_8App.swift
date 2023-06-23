5e6969c8bba310e4df062d09a3abc483daeb101c
Sampada <sampada>
2023-06-23 10:57:16 -0700

    Initial Commit

---
+++ lesson 8/lesson_8App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson_8App.swift
+//  lesson 8
+//
+//  Created by Sampada Baral on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct lesson_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
